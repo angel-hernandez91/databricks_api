73fcbbf5eeab21a5d0e68c592384d4450e4c0dd5
Angel Hernandez <ahernandez0691@gmail.com>
2018-10-21 18:32:31 -0700

    base databricks class added

---
+++ databricksapi/Databricks.py
@@ -0,0 +1,56 @@
+import json
+import requests
+
+class Databricks:
+	def __init__(self):
+		self._token = 'Bearer {}'.format(os.environ['DATABRICKS_TOKEN'])
+		self._headers ={'content_type': 'application/json', 'authorization': self._token}
+		self._api_suffix = '/api/2.0/'
+
+	def _url_sanitize(self, url):
+		if url[-1] == '/':
+			return url[0:-1]
+		else:
+			return url
+
+	def _set_url(self, url, endpoint):
+		return '{}{}{}'.format(self._url_sanitize(url), self._api_suffix, endpoint)
+
+	def _merge_dict(self, *args):
+		merged = args[0].copy()
+		for d in args[1::]:
+			merged.update(d)
+		return merged
+
+	def _json_flattener(self, start_dict):
+	    out = {}
+
+	    def flatten(temp_dict, name=''):
+	        if isinstance(temp_dict, dict):
+	            for key in temp_dict:
+	                flatten(temp_dict[key], key)
+	        else:
+	            out[name] = temp_dict
+
+	    flatten(start_dict)
+	    return out
+
+	# def _cluster_id_payload(self, cluster_name, url):
+	# 	cluster_map = GetClusterList(url).getClusterList()
+		
+	# 	payload = {
+	# 		"cluster_id": cluster_map[cluster_name.lower()]
+	# 	}
+	# 	return payload
+
+	# def _start_job_monitor(self, url, cluster_name, json_obj):
+	
+	# 	if json_obj != {}:
+	# 		cluster_monitor = ClusterMonitor(url).clusterMonitor(cluster_name)
+	# 		#running job
+	# 		run_id = json_obj['run_id']
+	# 		monitor = JobMonitor(url)
+	# 		start_monitor = monitor.jobMonitor(run_id)
+	# 		return start_monitor
+	# 	else:
+	# 		return "Job Failed to Start"
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as rm:
+	long_description = rm.read()
+
+setuptools.setup(
+	name='databricksapi',
+	version='0.1',
+	description='Python Databricks API wrapper using requests module',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	author='Angel Hernandez',
+	url='https://github.com/angel-hernandez91/databricks_api',
+	author_email='ahernandez0691@gmail.com',
+	license='MIT',
+	packages=setuptools.find_packages(),
+	zip_safe=False,
+	classifiers= [
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent'
+	]
+
+
+	)
