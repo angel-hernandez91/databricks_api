056bc246b92607d4d142c6550fb6480c3ff5790c
Angel Hernandez <ahernandez0691@gmail.com>
2018-10-30 23:55:59 -0700

    added Instance Profiles API

---
+++ databricksapi/InstanceProfile.py
@@ -0,0 +1,35 @@
+import Databricks
+
+class InstanceProfile(Databricks.Databricks):
+	def __init__(self, url):
+		super().__init__(self)
+		self._url = url
+		self._api_type = 'instance-profiles'
+
+	def addProfile(self, profile_arn, skip_validation=None):
+		endpoint = 'add'
+		url = self._set_url(self._url, self._api_type, endpoint)
+
+		payload = {
+			'instance_profile_arn': profile_arn
+		}
+
+		if skip_validation is not None:
+			payload['skip_validation'] = skip_validation
+
+		return self._post(url, payload)
+
+	def listProfiles(self):
+		endpoint = 'list'
+		url = self._set_url(self._url, self._api_type, endpoint)
+
+		return self._post(url, payload)
+
+	def removeProfile(self):
+		endpoint = 'remove'
+		url = self._set_url(self._url, self._api_type, endpoint)
+
+		return self._post(url, payload)
+
+
+
