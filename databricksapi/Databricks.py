eb31d11afb05ccba53116c8f2bd0fd723429b4f2
Angel Hernandez <ahernandez0691@gmail.com>
2018-12-12 22:34:17 -0800

    accept multipart/form-data + required import os

--- databricksapi/Databricks.py
+++ databricksapi/Databricks.py
@@ -1,10 +1,11 @@
 import json
 import requests
+import os
 
 class Databricks:
 	def __init__(self):
 		self._token = 'Bearer {}'.format(os.environ['DATABRICKS_TOKEN'])
-		self._headers ={'content_type': 'application/json', 'authorization': self._token}
+		self._headers = {'content_type': 'application/json', 'authorization': self._token}
 		self._api_suffix = '/api/2.0/'
 
 	def _url_sanitize(self, url):
@@ -35,34 +36,25 @@ class Databricks:
 		flatten(start_dict)
 		return out
 
-	def _post(self, url, payload=None):
+	def _post(self, url, payload=None, files=None):
+		headers = {'content_type': 'multipart/form-data', 'authorization': self._token}
 		if payload is None:
-			return requests.post(url, headers=self._headers).json()
+			if files is None:
+				return requests.post(url, headers=self._headers).json()
+			else:
+				
+				return requests.post(url, headers=headers, files=files).json()
 		else:
-			return requests.post(url, data=json.dumps(payload), headers=self._headers).json()
+			if files is not None:
+
+				return requests.post(url, data=payload, headers=headers, files=files).json()
+			else:
+				return requests.post(url, data=json.dumps(payload), headers=self._headers).json()
+
+
 
 	def _get(self, url, payload=None):
 		if payload is None:
-			return requests.post(url, headers=self._headers).json()
+			return requests.get(url, headers=self._headers).json()
 		else:
 			return requests.get(url, data=json.dumps(payload), headers=self._headers).json()
-
-	# def _cluster_id_payload(self, cluster_name, url):
-	# 	cluster_map = GetClusterList(url).getClusterList()
-		
-	# 	payload = {
-	# 		"cluster_id": cluster_map[cluster_name.lower()]
-	# 	}
-	# 	return payload
-
-	# def _start_job_monitor(self, url, cluster_name, json_obj):
-	
-	# 	if json_obj != {}:
-	# 		cluster_monitor = ClusterMonitor(url).clusterMonitor(cluster_name)
-	# 		#running job
-	# 		run_id = json_obj['run_id']
-	# 		monitor = JobMonitor(url)
-	# 		start_monitor = monitor.jobMonitor(run_id)
-	# 		return start_monitor
-	# 	else:
-	# 		return "Job Failed to Start"
