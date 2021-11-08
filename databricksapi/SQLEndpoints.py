e17de71b37018af75ab9b2f7d0b0cd602e171261
lotnikov <lotnikov@gmail.com>
2021-11-08 22:48:28 +0300

    Update SQLEndpoints.py

--- databricksapi/SQLEndpoints.py
+++ databricksapi/SQLEndpoints.py
@@ -1,248 +1,94 @@
 from . import Databricks
 
-class ClusterTypeNotSupportedExpcetion(Exception):
-	def __init__(self, cluster_type):
-		Exception.__init__(self, "The cluster type '{}' is not supported. Use either 'existing' or 'new'.".format(cluster_type))
-
-class TaskTypeNotSupportedException(Exception):
-	def __init__(self, task_type):
-			Exception.__init__(self,	"The task type '{}' is not supported. Use either 'notebook', 'jar', 'python', or 'submit'.".format(task_type))
-
-class RunTypeNotSupportedException(Exception):
-	def __init__(self, run_type):
-		Exception.__init__(self, "The run type '{}' is not supported. Use either 'active' or 'completed'.".format(run_type))
-
-class Jobs(Databricks.Databricks):
+class SQLEndpoints(Databricks.Databricks):
 	def __init__(self, url, token=None):
 		super().__init__(token)
 		self._url = url
-		self._api_type = 'jobs'
+		self._api_type = 'sql'
 
-	def createJob(self, cluster, cluster_type, task, task_type, name, 
-					libraries=None, email_notications=None, timeout_seconds=None,
-					max_retries=None, min_retry_interval_millis=None, retry_on_timeout=None,
-					schedule=None, max_concurrent_runs=None):
-		endpoint = 'create'
+	def createEndpoint(self, name, cluster_size, min_num_clusters=1, max_num_clusters=1, auto_stop_mins=10, spot_instance_policy=None,
+			         enable_photon=True, enable_serverless_compute=False, tags=None):
+		endpoint = 'endpoints'
 		url = self._set_url(self._url, self._api_type, endpoint)
 
 		payload = {
-			'name': name
-		}
-		if cluster_type.lower() == 'existing':
-			payload['existing_cluster_id'] = cluster
-		elif cluster_type.lower() == 'new':
-			payload['new_cluster'] = cluster
-		else:
-			raise ClusterTypeNotSupportedExpcetion(cluster_type)
-
-		if task_type.lower() == 'notebook':
-			payload['notebook_task'] = task
-		elif task_type.lower() == 'jar':
-			payload['spark_jar_task'] = task
-		elif task_type.lower() == 'python':
-			payload['spark_python_task'] = task
-		elif task_type.lower() == 'submit':
-			payload['spark_submit_task'] = task
-		else:
-			raise TaskTypeNotSupportedException(task_type)
-
-		if libraries is not None:
-			payload['libraries'] = libraries
-		if email_notications is not None:
-			payload['email_notications'] = email_notications
-		if timeout_seconds is not None:
-			payload['timeout_seconds'] = timeout_seconds
-		if max_retries is not None:
-			payload['max_retries'] = max_retries
-		if min_retry_interval_millis is not None:
-			payload['min_retry_interval_millis'] = min_retry_interval_millis
-		if retry_on_timeout is not None:
-			payload['retry_on_timeout'] = retry_on_timeout
-		if schedule is not None:
-			payload['schedule'] = schedule
-		if max_concurrent_runs is not None:
-			payload['max_concurrent_runs'] = max_concurrent_runs
-
+			"name": name,
+			"cluster_size": cluster_size,
+			"min_num_clusters": min_num_clusters,
+			"max_num_clusters": max_num_clusters,
+			"spot_instance_policy":spot_instance_policy,
+			"enable_photon": enable_photon,
+			"enable_serverless_compute": enable_serverless_compute,
+			"tags": tags
+			   }
 		return self._post(url, payload)
 
-	def listJobs(self):
-		endpoint = 'list'
+	def listEndpoints(self):
+		endpoint = 'endpoints'
 		url = self._set_url(self._url, self._api_type, endpoint)
 		
 		return self._get(url)
 
-	def deleteJob(self, job_id):
-		endpoint = 'delete'
+	def deleteEndpoint(self, endpoint_id):
+		endpoint = 'endpoints/'+endpoint_id
 		url = self._set_url(self._url, self._api_type, endpoint)
 
-		payload = {
-			'job_id': job_id
-		}
+		return self._delete(url, payload)
+
+	def getEndpoint(self, endpoint_id):
+		endpoint = 'endpoints/'+endpoint_id
+		url = self._set_url(self._url, self._api_type, endpoint)
+
+		return self._get(url)
+	
+	def updateEndpoint(self, endpoint_id, name=None, cluster_size=None, min_num_clusters=None, max_num_clusters=None, auto_stop_mins=None, tags=Noneб
+			  spot_instance_policy=None, enable_photon=None, enable_serverless_compute=None):
+		endpoint = 'endpoints/'+endpoint_id+'/edit'
+		url = self._set_url(self._url, self._api_type, endpoint)
+		
+		payload = {}
+		if endpoint_id != None : payload["id"] = endpoint_id
+		if name != None : payload["name"] = name
+		if cluster_size != None : payload["cluster_size"] = cluster_size
+		if min_num_clusters != None : payload["min_num_clusters"] = min_num_clusters
+		if max_num_clusters != None : payload["max_num_clusters"] = max_num_clusters
+		if auto_stop_mins != None : payload["auto_stop_mins"] = auto_stop_mins
+		if tags != None : payload["tags"] = tags
+		if spot_instance_policy != None : payload["spot_instance_policy"] = spot_instance_policy
+		if enable_photon != None : payload["enable_photon"] = enable_photon
+		if enable_serverless_compute != None : payload["enable_serverless_compute"] = enable_serverless_compute
 
 		return self._post(url, payload)
 
-	def updateJob(self, job_settings):
-		endpoint = 'update'
+
+	def startEndpoint(self, endpoint_id):
+		endpoint = 'endpoints'+endpoint_id+'/start'
 		url = self._set_url(self._url, self._api_type, endpoint)
 
-		payload = job_settings
+		return self._post(url)
+	
+	def stopEndpoint(self, endpoint_id):
+		endpoint = 'endpoints'+endpoint_id+'/stop'
+		url = self._set_url(self._url, self._api_type, endpoint)
 
-		return self._post(url, payload)
-
-	def batchDelete(self, *args):
-		for job in args:
-			self.deleteJob(job)
-
-	def deleteAllJobs(self):
-		pass
-
-	def mapJobNames(self):
-		pass
-
-	def getJob(self, job_id):
-		endpoint = 'get/?job_id='+str(job_id)
+		return self._post(url)
+	
+	def listGlobalEndpoints(self):
+		endpoint = 'config/endpoints'
 		url = self._set_url(self._url, self._api_type, endpoint)
 
 		return self._get(url)
 
-	def resetJob(self, job_id, new_settings):
-		endpoint = 'reset'
+	def updateGlobalEndpoints(self, security_policy, data_access_config, instance_profile_arn):
+		endpoint = 'config/endpoints'
 		url = self._set_url(self._url, self._api_type, endpoint)
 
 		payload = {
-			'job_id': job_id,
-			'new_settings': new_settings
+			"security_policy": security_policy,
+			"data_access_config": data_access_config,
+			"instance_profile_arn": instance_profile_arn
 		}
 
-		return self._post(url, payload)
+		return self._put(url, payload)
 
-	def runJob(self, job_id, job_type, params):
-		endpoint = 'run-now'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'job_id': job_id,
-		}
-
-		if job_type == 'notebook':
-			payload['notebook_params'] = params
-		elif job_type == 'jar':
-			payload['jar_params'] = params
-		elif job_type == 'python':
-			payload['python_params'] = params
-		elif job_type == 'submit':
-			payload['spark_submit_params'] = params
-		else:
-			raise TaskTypeNotSupportedException(job_type)	
-
-		return self._post(url, payload)
-
-	def runsSubmit(self, run_name, cluster, task, cluster_type, task_type, libraries=None, timeout_seconds=None):
-		endpoint = 'runs/submit'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'run_name': run_name,
-		}
-
-		if cluster_type == 'existing':
-			payload['existing_cluster_id'] = cluster
-		elif cluster_type == 'new':
-			payload['new_cluster'] = cluster
-		else:
-			raise ClusterTypeNotSupportedExpcetion(cluster_type)
-
-		#set the tasks
-		if task_type.lower() == 'notebook':
-			payload['notebook_task'] = task
-		elif task_type.lower() == 'jar':
-			payload['spark_jar_task'] = task
-		elif task_type.lower() == 'python':
-			payload['spark_python_task'] = task
-		elif task_type.lower() == 'submit':
-			payload['spark_submit_task'] = task
-		else:
-			raise TaskTypeNotSupportedException(task_type)
-
-		if libraries is not None:
-			payload['libraries'] = libraries
-
-		if timeout_seconds is not None:
-			payload['timeout_seconds'] = timeout_seconds
-
-		return self._post(url, payload)
-
-	def runsList(self, run_type, job_id=None, offset=None, limit=None):
-
-		if job_id is not None:
-			endpoint = 'runs/list?job_id='+str(job_id)
-			
-		if offset is not None:
-			endpoint += '&offset='+str(offset)
-
-		if limit is not None:
-			endpoint += '&limit='+str(limit)
-
-		if run_type.lower() == 'active':
-			endpoint += '&active_only=true'
-		elif run_type.lower() == 'completed':
-			endpoint += '&completed_only=true'
-		elif run_type.lower() == 'all':
-			pass
-		else:
-			raise RunTypeNotSupportedException(run_type)
-		
-		url = self._set_url(self._url, self._api_type, endpoint)
-		
-		return self._get(url)
-
-	def runsGet(self, run_id):
-		endpoint = 'runs/get'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'run_id': run_id
-		}
-
-		return self._get(url, payload)
-
-	def runsExport(self, run_id, views_to_export):
-		endpoint = 'runs/export'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'run_id': run_id,
-			'views_to_export': views_to_export
-		}
-
-		return self._get(url, payload)
-
-	def runsCancel(self, run_id):
-		endpoint = 'runs/cancel'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'run_id': run_id
-		}
-
-		return self._post(url, payload)
-
-	def runsGetOutput(self, run_id):
-		endpoint = 'runs/get-output'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'run_id': run_id
-		}
-
-		return self._get(url, payload)
-
-	def runsDelete(self, run_id):
-		endpoint = 'runs/delete'
-		url = self._set_url(self._url, self._api_type, endpoint)
-
-		payload = {
-			'run_id': run_id
-		}
-
-		return self._post(url, payload)
+	
